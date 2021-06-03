8f80005e5974cc1fb1001a81b3a47b5923297a39
Philip Joubert <78829754+Mr-Fennie@users.noreply.github.com>
2021-06-03 12:40:23 +0200

    Major .css update
    Added: 
    Logo
    cover image
    
    updated .js

--- PizzaRecipeCalculator/master.js
+++ PizzaRecipeCalculator/master.js
@@ -1,6 +1,6 @@
 
 
-function weightConverter(valNum) {
+const weightConverter = (valNum) => {
   document.getElementById("outputWater").innerHTML=valNum*(64.5/100);
   document.getElementById("outputSugar").innerHTML=valNum*(2.6/100);
   document.getElementById("outputSalt").innerHTML=valNum*(1.6/100);
